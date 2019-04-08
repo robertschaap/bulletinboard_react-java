bb3cab2627caf8458e8f2b71811eba83617728b6
robertschaap <robert.schaap@mac.com>
2019-04-08 23:03:50 +0200

    server: run basic server in App

--- server/src/main/java/com/bulletinboard/App.java
+++ server/src/main/java/com/bulletinboard/App.java
@@ -1,7 +1,12 @@
 package com.bulletinboard;
 
+import static spark.Spark.*;
+
 public class App {
   public static void main( String[] args ) {
-      System.out.println( "Hello World!" );
+      int port = 4567;
+      port(port);
+      System.out.println("Running on http://localhost:" + port);
+      get("/", (req, res) -> "Hello World!");
   }
 }
