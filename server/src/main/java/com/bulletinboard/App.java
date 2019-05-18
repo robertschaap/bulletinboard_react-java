55bf5387597d8c205cdb305345b07748abd7db89
robertschaap <robert.schaap@mac.com>
2019-05-18 14:23:50 +0200

    server: moved port integer to App class attribute

--- server/src/main/java/com/bulletinboard/App.java
+++ server/src/main/java/com/bulletinboard/App.java
@@ -3,8 +3,9 @@ package com.bulletinboard;
 import static spark.Spark.*;
 
 public class App {
+  private static Integer port = 4567;
+
   public static void main( String[] args ) {
-      int port = 4567;
       port(port);
       System.out.println("Running on http://localhost:" + port);
       get("/", (req, res) -> "Hello World!");
