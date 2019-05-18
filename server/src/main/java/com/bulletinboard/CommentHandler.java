af9f435c7fe06e86ed800bf7f01b14f0ad427fc7
robertschaap <robert.schaap@mac.com>
2019-05-18 18:06:34 +0200

    server: add dummy data to CommentHandler

--- server/src/main/java/com/bulletinboard/CommentHandler.java
+++ server/src/main/java/com/bulletinboard/CommentHandler.java
@@ -1,5 +1,6 @@
 package com.bulletinboard;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import spark.Request;
@@ -9,6 +10,14 @@ public class CommentHandler {
   public static String getComments(Request req, Response res) {
     ApiResponse apiResponse = new ApiResponse();
     HashMap<String, Object> data = new HashMap<>();
+
+    ArrayList<Comment> comments = new ArrayList<Comment>();
+    comments.add(new Comment(1, "title", "body", "name"));
+    comments.add(new Comment(2, "title", "body", "name"));
+    comments.add(new Comment(3, "title", "body", "name"));
+    comments.add(new Comment(4, "title", "body", "name"));
+    comments.add(new Comment(5, "title", "body", "name"));
+    data.put("comments", comments);
 
     apiResponse.setStatus("success");
     apiResponse.setData(data);
