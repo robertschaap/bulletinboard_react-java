9e70c04c810b7afe26de630608db0c3e0e8e17bd
robertschaap <robert.schaap@mac.com>
2019-05-19 19:21:09 +0200

    server: add postComments method to CommentHandler

--- server/src/main/java/com/bulletinboard/CommentHandler.java
+++ server/src/main/java/com/bulletinboard/CommentHandler.java
@@ -1,7 +1,12 @@
 package com.bulletinboard;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import spark.Request;
 import spark.Response;
@@ -26,4 +31,21 @@ public class CommentHandler {
 
     return apiResponse.toJson();
   };
+
+  public static String postComments(Request req, Response res) {
+    res.type("application/json");
+
+    ApiResponse apiResponse = new ApiResponse();
+    Type type = new TypeToken<Map<String, String>>(){}.getType();
+    Map<String, String> map = new Gson().fromJson(req.body(), type);
+
+    if (map.containsKey("title") && map.containsKey("body") && map.containsKey("name")) {
+      apiResponse.setStatus("success");
+      return apiResponse.toJson();
+    }
+
+    apiResponse.setStatus("error");
+    apiResponse.setMessage("error");
+    return apiResponse.toJson();
+  }
 }
