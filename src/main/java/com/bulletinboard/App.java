fd2700637c792bfe41154a5cba0b5355ecfd2261
robertschaap <robert.schaap@mac.com>
2020-08-20 17:29:21 +0200

    App: fix styling

--- src/main/java/com/bulletinboard/App.java
+++ src/main/java/com/bulletinboard/App.java
@@ -5,11 +5,11 @@ import static spark.Spark.*;
 public class App {
   private static Integer port = 4000;
 
-  public static void main( String[] args ) {
-      port(port);
-      System.out.println("Running on http://localhost:" + port);
+  public static void main(String[] args) {
+    port(port);
+    System.out.println("Running on http://localhost:" + port);
 
-      get("/api/comments", CommentHandler::getComments);
-      post("/api/comments", CommentHandler::postComments);
+    get("/api/comments", CommentHandler::getComments);
+    post("/api/comments", CommentHandler::postComments);
   }
 }
